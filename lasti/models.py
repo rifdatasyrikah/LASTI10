20b6171114f63e644705a049a925150ab51a2809
rifda <rifda.tasyrikah@gmail.com>
2022-12-04 12:22:08 +0700

    relationship jadi one-to-many dan many-to-one

--- lasti/models.py
+++ lasti/models.py
@@ -7,10 +7,11 @@ class Instansi(Base):
     __tablename__="Instansi"
     id_instansi=Column(Integer,primary_key=True,index=True)
     nama_instansi=Column(String)
-    #one-to-one relationship dengan requester
-    requester=relationship("Requester",back_populates="instansi_column",uselist=False)
-    #one-to-one relationship dengan provider
-    provider=relationship("Provider",back_populates="instansi_column_2",uselist=False)
+
+    #one-to-many relationship dengan provider
+    provider=relationship("Provider",back_populates="instansi_column")
+    #one-to-many relationship dengan publisher
+    publisher=relationship("Publisher",back_populates="instansi_column")
 
 class Requester(Base):
     __tablename__="Requester"
@@ -23,10 +24,10 @@ class Requester(Base):
     #one-to-one relationship dengan instansi
     # instansi_column=relationship("Instansi",back_populates="requester")
 
-    #one-to-one relationship dengan req_penggunaan_API
-    req_penggunaan_2=relationship("Req_penggunaan_API",back_populates="requester2")
-    #one-to-one relationship dengan req_penyediaan_API
-    req_penyediaan=relationship("Req_penyediaan_API",back_populates="requester3")
+    #one-to-many relationship dengan req_penggunaan_API
+    req_penggunaan=relationship("Req_penggunaan_API",back_populates="requester")
+    #one-to-many relationship dengan req_penyediaan_API
+    req_penyediaan=relationship("Req_penyediaan_API",back_populates="requester")
 
 class Provider(Base):
     __tablename__="Provider"
@@ -36,10 +37,10 @@ class Provider(Base):
     NIP=Column(Integer)
     instansi=Column(String,ForeignKey("Instansi.id_instansi"))
 
-    #one-to-one relationship dengan instansi
+    #many-to-one relationship dengan instansi
     instansi_column=relationship("Instansi",back_populates="provider")
-    #one-to-one relationship dengan API
-    API4=relationship("API",back_populates="provider",uselist=False)
+    #one-to-many relationship dengan API
+    API=relationship("API",back_populates="provider")
 
 class Publisher(Base):
     __tablename__="Publisher"
@@ -49,10 +50,10 @@ class Publisher(Base):
     NIP=Column(Integer)
     instansi=Column(String,ForeignKey("Instansi.id_instansi"))
 
-    #one-to-one relationship dengan instansi
-    instansi_column_2=relationship("Instansi",back_populates="publisher")
-    #one-to-one relationship dengan req_publikasi_API
-    req_publikasi=relationship("Req_publikasi_API",back_populates="publisher",uselist=False)
+    #many-to-one relationship dengan instansi
+    instansi_column=relationship("Instansi",back_populates="publisher")
+    #one-to-many relationship dengan req_publikasi_API
+    req_publikasi=relationship("Req_publikasi_API",back_populates="publisher")
 
 
 class Req_publikasi_API(Base):
@@ -61,10 +62,11 @@ class Req_publikasi_API(Base):
     id_publisher=Column(Integer,ForeignKey("Publisher.id_publisher"))
     id_api=Column(Integer,ForeignKey("API.id_api"))
     status_akses=Column(Boolean, default = False)
-    #one-to-one relationship dengan publisher
+    
+    #one-to-many relationship dengan publisher
     publisher=relationship("Publisher",back_populates="req_publikasi")
-    #one-to-one relationship dengan API
-    API=relationship("API",back_populates="req_publikasi2")
+    #one-to-many relationship dengan API
+    API=relationship("API",back_populates="req_publikasi")
 
 class API(Base):
     __tablename__="API"
@@ -76,21 +78,23 @@ class API(Base):
     alamat_akses=Column(String)
     file_adapter=Column(String)
     is_published=Column(Boolean, default = False)
-    #one-to-one relationship dengan request_publikasi_api
-    req_publikasi2=relationship("Request_publikasi_API",back_populates="API",uselist=False)
-    #one-to-one relationship dengan request_penggunaan_api
-    req_penggunaan=relationship("Request_penggunaan_API",back_populates="API2",uselist=False)
-    #one-to-one relationship dengan direktori
-    direktori=relationship("Direktori",back_populates="API3")
-    #one-to-one relationship dengan provider
-    provider=relationship("Provider",back_populates="API4")
+
+    #one-to-many relationship dengan request_publikasi_api
+    req_publikasi=relationship("Request_publikasi_API",back_populates="API")
+    #one-to-many relationship dengan request_penggunaan_api
+    req_penggunaan=relationship("Request_penggunaan_API",back_populates="API")
+    #many-to-one relationship dengan direktori
+    direktori=relationship("Direktori",back_populates="API")
+    #many-to-one relationship dengan provider
+    provider=relationship("Provider",back_populates="API")
 
 class Direktori(Base):
     __tablename__="Direktori"
     id_direktori=Column(Integer,primary_key=True,index=True)
     nama_direktori=Column(String)
-    #one-to-one relationship dengan API
-    API3=relationship("API",back_populates="direktori",uselist=False)
+
+    #one-to-many relationship dengan API
+    API=relationship("API",back_populates="direktori")
 
 class Req_penggunaan_API(Base):
     __tablename__="Request_Penggunaan_API"
@@ -99,10 +103,10 @@ class Req_penggunaan_API(Base):
     id_api=Column(Integer,ForeignKey("API.id_api"))
     status_akses=Column(Boolean, default = False)
 
-    #one-to-one relationship dengan API
-    API2=relationship("API",back_populates="req_penggunaan")
-    #one-to-one relationship dengan requester
-    requester2=relationship("Requester",back_populates="req_penggunaan_2")
+    #many-to-one relationship dengan API
+    API=relationship("API",back_populates="req_penggunaan")
+    #many-to-one relationship dengan requester
+    requester=relationship("Requester",back_populates="req_penggunaan")
     
 
 class Req_penyediaan_API(Base):
@@ -113,7 +117,7 @@ class Req_penyediaan_API(Base):
     spesifikasi=Column(String)
     status_konfirmasi=Column(Boolean, default = False)
 
-    #one-to-one relationship dengan requester
-    requester3=relationship("Requester",back_populates="req_penyediaan")
+    #many-to-one relationship dengan requester
+    requester=relationship("Requester",back_populates="req_penyediaan")
 
 
